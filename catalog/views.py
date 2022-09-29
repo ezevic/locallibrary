8879a6c430c16c55c69c7315bfc6c4eddaeff630
ezevic <ezequiel.vicente.2@gmail.com>
2022-09-29 09:14:44 -0300

    first commit

---
+++ catalog/views.py
@@ -0,0 +1,13 @@
+from multiprocessing import context
+from re import L
+from django.shortcuts import render
+from django.views.generic import View
+
+# Create your views here.
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+
+        }
+        return render(request, 'home.html', context)
