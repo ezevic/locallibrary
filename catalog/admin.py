be25712ae4a9f91dd464047c906db16c748553d8
ezevic <ezequiel.vicente.2@gmail.com>
2022-09-30 00:16:29 -0300

    added admin stuff

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,7 +5,48 @@ from django.contrib import admin
 from .models import Genre, Book, BookInstance, Author
 
 
-admin.site.register(Book)
-admin.site.register(Author)
+# Define the admin class
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name',
+                    'date_of_birth', 'date_of_death')
+
+    inlines = [BookInline]
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            "fields": ('book', 'imprint', 'id')
+        }),
+
+        ('Availability', {
+            "fields": ('status', 'due_back')
+        }
+
+        )
+    )
+
+
+
+
+
+# Register the admin class with the associated model
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
