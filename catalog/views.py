6aff8210fba00069f17d5c1c03a421720d2872d6
ezevic <ezequiel.vicente.2@gmail.com>
2022-10-02 16:51:08 -0300

    author detail view updated

--- catalog/views.py
+++ catalog/views.py
@@ -73,6 +73,10 @@ class AuthorListView(ListView):
 
 class AuthorDetailView(DetailView):
     model = Author
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.all().order_by('-title')
+        return context
 
 """
 La manera larga de escribir la vista de clase ListView es la siguiente
